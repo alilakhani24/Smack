29391240237bd64eae1c7251814d4b5395cd60f7
Ali <ali_lakhani@hotmail.com>
2019-08-28 17:36:59 -0400

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Ali Lakhani on 2019-08-28.
+//  Copyright © 2019 Ali Lakhani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
